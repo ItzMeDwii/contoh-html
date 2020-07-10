36f1d5ea67ead36b15f759699851291c09cc31fa
Satya Bagus Dwiatmaja <dwii5359@azalelnation.com>
2020-07-10 12:32:24 +0700

    Create script.js

---
+++ simple-bootstrap/script.js
@@ -0,0 +1,43 @@
+var btn = $(".btt");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+$('a[href*="#"]')
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function (event) {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        event.preventDefault();
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          function () {
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) {
+              return false;
+            } else {
+              $target.attr("tabindex", "-1");
+              $target.focus();
+            }
+          }
+        );
+      }
+    }
+  });
